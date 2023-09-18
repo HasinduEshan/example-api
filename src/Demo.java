03e9cc8970c144b58e8e25889b4b584c639811eb
HasinduEshan <hasindueshandesilva@gmail.com>
2023-09-18 11:42:15 +0530

    stream.forEach

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Demo {
     public static void main(String[] args) throws Exception {
+        List<Integer> list = Arrays.asList(10, 20, 30, 40, 21, 11, 34, 54, 67, 87, 45, 34);
 
+        for (Integer integer:list) {
+            System.out.print(integer+", ");
+        }
+
+        System.out.println();
+        list.stream().forEach(n-> System.out.print(n+", "));
+
+        System.out.println();
+        list.stream().forEach(n-> System.out.print((n*2)+", "));
     }
 }
